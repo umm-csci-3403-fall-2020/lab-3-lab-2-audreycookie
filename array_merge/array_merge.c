23b357d5348b59f98ce3c31847c03f27517eb494
Cookie Vang <vang2660@morris.umn.edu>
2020-09-19 04:09:39 -0500

    Add comments about how to approach algorithm

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,6 +1,15 @@
 #include "array_merge.h"
 
+//num_arrays is the number of arrays that are in values
+//sizes is an array that has the sizes of the corresponding array in values
+//values is an array of arrays with the number of arrays and size each array determined by num_arrays and sizes
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
+  int sizesIndex = 0;
+  for (int i = 0; i < num_arrays; ++i) {
+	//use calloc to create an array based on the sizesIndex. Creating num_arrays many arrays
+	//++sizesIndex;
+  }
+  
   return sizes;
-}
+
+ 
