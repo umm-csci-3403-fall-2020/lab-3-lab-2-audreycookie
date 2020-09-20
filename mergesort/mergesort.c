104dd79f0947eee49d30e707db24d17c88bd4ae0
Cookie Vang <vang2660@morris.umn.edu>
2020-09-19 22:49:50 -0500

    Try to implement merge sort, have error

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,7 +1,58 @@
 #include "mergesort.h"
+#include <stdbool.h>
+#include <cstdlib>
 
-void mergesort(int, int*) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
+//bool stands for boolean in C
+bool needsSorting(int rangeSize){
+  return rangeSize >= 2;
+}
+
+void mergeRanges(int* values, int startIndex, int midPoint, int endIndex){
+  int rangeSize = endIndex - startIndex;
+  //result = (char*) calloc(len+1, sizeof(char));
+  char* destination;
+  destination = (char*) calloc(rangeSize+1, sizeof(char));
+  int firstIndex = startIndex;
+  int secondIndex = midPoint;
+  int copyIndex = 0;
+  while (firstIndex < midPoint && secondIndex < endIndex) {
+  	if (values[firstIndex] < values[secondIndex]){
+	  destination[copyIndex] = values[firstIndex];
+	  ++firstIndex;
+	}
+	else {
+	destination[copyIndex] = values[secondIndex];
+	++secondIndex;
+	}
+	++copyIndex;
+  }
+  while (firstIndex < midPoint){
+	destination[copyIndex] = values[firstIndex];
+	++copyIndex;
+	++firstIndex;
+  }
+  while (secondIndex < endIndex){
+  	destination[copyIndex] = values[secondIndex];
+	++copyIndex;
+	++secondIndex;
+  }
+  for (int i = 0; i < rangeSize; ++i){
+	values[i + startIndex] = destination[i];
+  }
+  free(destination);
+}
+
+void mergesortRange(int* values, int startIndex, int endIndex) {
+  int rangeSize = endIndex - startIndex;
+  if (needsSorting(rangeSize)){
+	int midPoint = (startIndex + endIndex) / 2;
+	mergesortRange(values, startIndex, midPoint);
+	mergesortRange(values, midPoint, endIndex);
+	mergeRanges(values, startIndex, midPoint, endIndex);
+  }
+}
+
+void mergesort(int, int* values) {
+  mergesortRange(values, 0, sizeof(values));
   return;
 }
